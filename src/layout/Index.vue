3e155cd2315d78b688a543dcc21a055e01b79357
yian <915826506@qq.com>
2024-11-11 15:33:51 +0800

    feat: 左侧导航菜单制作

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,24 +1,33 @@
 <template>
   <el-container class="layout">
-    <el-aside width="200px" class="asside">Aside</el-aside>
+    <el-aside width="200px" class="asside">
+      <menu-bar></menu-bar>
+    </el-aside>
     <el-container>
-      <el-header class="header">Header</el-header>
+      <el-header class="header">
+        <Header></Header>
+      </el-header>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
+<script setup lang="ts">
+import Header from './header/Header.vue'
+import MenuBar from './menu/MenuBar.vue'
+</script>
+
 <style lang="scss">
 .layout {
   height: 100%;
   .asside {
-    background-color: blueviolet;
+    background-color: #304156;
   }
   .header {
     background-color: #009688;
   }
   .main {
-    background-color: darkcyan;
+    background-color: darkgoldenrod;
   }
 }
 </style>
---
+++ src/layout/menu/MenuBar.vue
@@ -0,0 +1,122 @@
+<template>
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    unique-opened
+    background-color="#304156"
+  >
+    <menu-item :menuList="menuList"></menu-item>
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+import MenuItem from '@/layout/menu/MenuItem.vue'
+let menuList = reactive([
+  {
+    path: '/system',
+    component: 'Layout',
+    name: 'system',
+    meta: {
+      title: '系统管理',
+      icon: 'Setting',
+      roles: ['sys:manage']
+    },
+    children: [
+      {
+        path: '/userList',
+        component: '/system/User/UserList',
+        name: 'userList',
+        meta: {
+          title: '用户管理',
+          icon: 'UserFilled',
+          roles: ['sys:user']
+        }
+      },
+      {
+        path: '/roleList',
+        component: '/system/Role/RoleList',
+        name: 'roleList',
+        meta: {
+          title: '角色管理',
+          icon: 'Wallet',
+          roles: ['sys:role']
+        }
+      },
+      {
+        path: '/menuList',
+        component: '/system/Menu/MenuList',
+        name: 'menuList',
+        meta: {
+          title: '菜单管理',
+          icon: 'Menu',
+          roles: ['sys:menu']
+        }
+      }
+    ]
+  },
+  {
+    path: '/goodsRoot',
+    component: 'Layout',
+    name: 'goodsRoot',
+    meta: {
+      title: '商品管理',
+      icon: 'Setting',
+      roles: ['sys:goodsRoot']
+    },
+    children: [
+      {
+        path: '/category',
+        component: '/goods/Category',
+        name: 'category',
+        meta: {
+          title: '物资类型',
+          icon: 'UserFilled',
+          roles: ['sys:category']
+        }
+      },
+      {
+        path: '/goodsList',
+        component: '/goods/GoodsList',
+        name: 'goodsList',
+        meta: {
+          title: '商品信息',
+          icon: 'Wallet',
+          roles: ['sys:goodsList']
+        }
+      }
+    ]
+  }
+])
+</script>
+
+<style scoped lang="scss">
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 240px;
+  min-height: 400px;
+}
+
+.el-menu {
+  border-right: none;
+}
+
+:deep(.el-sub-menu .el-sub-menu__title) {
+  color: #f4f4f5 !important;
+}
+
+:deep(.el-menu .el-menu-item) {
+  color: #bfcbd9;
+}
+
+:deep(.el-menu-item.is-active) {
+  color: #409eff !important;
+}
+
+:deep(.is-opened .el-menu-item) {
+  background-color: #1f2d3d !important;
+}
+
+:deep(.el-menu-item:hover) {
+  background-color: #001528 !important;
+}
+</style>
