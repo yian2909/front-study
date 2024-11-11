74239986bf550e6b1fa4df0ab8bb548cd01dfa6d
yian <915826506@qq.com>
2024-11-11 18:53:08 +0800

    feat: 弹框组件封装

--- src/views/system/Menu/MenuList.vue
+++ src/views/system/Menu/MenuList.vue
@@ -1,7 +1,28 @@
 <template>
-  <h2>菜单管理</h2>
+  <div>
+    <h2>菜单管理</h2>
+    <el-button type="primary" size="default" @click="addBtn">新增</el-button>
+    <sys-dialog
+      title="菜单管理"
+      :visible="dialog.visible"
+      :width="dialog.width"
+      :height="dialog.height"
+      @onClose="onClose"
+      @onConfirm="onConfirm"
+    >
+      <template v-slot:content>
+        <div>我是弹框</div>
+      </template>
+    </sys-dialog>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script setup lang="ts">
+import SysDialog from '@/components/SysDialog.vue'
+import useDialog from '@/hooks/useDialog'
+//解构弹框属性
+const { dialog, onClose, onConfirm } = useDialog()
+const addBtn = () => {
+  dialog.visible = true
+}
+</script>
--- src/views/system/User/UserList.vue
+++ src/views/system/User/UserList.vue
@@ -1,7 +1,42 @@
 <template>
   <h2>用户管理</h2>
+  <div>
+    <el-button type="primary" size="default" @click="addBtn">新增</el-button>
+    <sys-dialog
+      :title="dialog.title"
+      :visible="dialog.visible"
+      :width="dialog.width"
+      :height="dialog.height"
+      @onClose="onClose"
+      @onConfirm="onConfirm"
+    >
+      <template v-slot:content>
+        <div>我是弹框</div>
+      </template>
+    </sys-dialog>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SysDialog from '@/components/SysDialog.vue'
+import { reactive } from 'vue'
 
-<style scoped></style>
+const dialog = reactive({
+  title: '用户管理',
+  width: 600,
+  height: 300,
+  visible: false
+})
+
+const addBtn = () => {
+  dialog.visible = true
+}
+
+const onClose = () => {
+  dialog.visible = false
+}
+
+const onConfirm = () => {
+  dialog.visible = false
+}
+</script>
