5b51087dd998ec4539977c12bf5e51326c0b4a5c
yian <915826506@qq.com>
2024-11-11 14:55:36 +0800

    feat: 路由安装

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,38 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import prettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+  ...pluginVue.configs['flat/essential'],
   {
-    files: ["**/*.vue"],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } }
   },
   {
     ignores: [
-      "node_modules/*",
-      "dist/*",
-      "*.css",
-      "*.jpg",
-      "*.jpeg",
-      "*.png",
-      "*.gif",
-      "*.d.ts",
-    ],
+      'node_modules/*',
+      'dist/*',
+      '*.css',
+      '*.jpg',
+      '*.jpeg',
+      '*.png',
+      '*.gif',
+      '*.d.ts'
+    ]
   },
   {
     rules: {
-      "no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "vue/multi-word-component-names": "off",
-    },
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'vue/multi-word-component-names': 'off'
+    }
   },
-  prettierRecommended,
-];
+  prettierRecommended
+]
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  }
 })
