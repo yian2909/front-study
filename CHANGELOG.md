4616063914c517998c251142d178168a3198577f
yian <915826506@qq.com>
2024-11-11 22:51:00 +0800

    feat: 新增角色页面制作

---
+++ CHANGELOG.md
@@ -0,0 +1,44 @@
+## 0.0.0 (2024-11-11)
+
+* feat: 安装 element-plus dd100a7
+* feat: 安装 pinia 658004c
+* feat: 菜单收缩制作 c6791fd
+* feat: 菜单logo制作 9e51dc0
+* feat: 弹框组件封装 7423998
+* feat: 关闭选项卡功能实现 897d4ce
+* feat: 路由安装 5b51087
+* feat: 路由配置与页面配置 3f9c646
+* feat: 面包屑导航制作 1c03aeb
+* feat: 选项卡关闭标签制作 d038f58
+* feat: 引入axios 754eaa4
+* feat: 主界面布局 7554483
+* feat: 左侧导航菜单制作 3e155cd
+* feat: pinia 持久化操作 891176a
+* feat: tabs 选项卡制作 7126bcb
+* chore: 项目初始化 54fcb17
+
+
+
+# 0.0.0 (2024-11-11)
+
+
+### Features
+
+* 安装 element-plus dd100a7
+* 安装 pinia 658004c
+* 菜单收缩制作 c6791fd
+* 菜单logo制作 9e51dc0
+* 弹框组件封装 7423998
+* 关闭选项卡功能实现 897d4ce
+* 路由安装 5b51087
+* 路由配置与页面配置 3f9c646
+* 面包屑导航制作 1c03aeb
+* 选项卡关闭标签制作 d038f58
+* 引入axios 754eaa4
+* 主界面布局 7554483
+* 左侧导航菜单制作 3e155cd
+* pinia 持久化操作 891176a
+* tabs 选项卡制作 7126bcb
+
+
+
--- src/views/system/Role/RoleList.vue
+++ src/views/system/Role/RoleList.vue
@@ -1,7 +1,99 @@
 <template>
-  <h2>角色管理</h2>
+  <el-main>
+    <!-- 搜索栏 -->
+    <el-form :model="searchParm" :inline="true" size="default"
+      ><el-form-item>
+        <el-input
+          placeholder="请输入关键字"
+          v-model="searchParm.roleName"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button icon="Search" @click="searchBtn">搜索</el-button>
+        <el-button icon="Close" type="danger" plain @click="resetBtn"
+          >重置</el-button
+        >
+        <el-button icon="Plus" type="primary" @click="addBtn">新增</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 新增、编辑弹框 -->
+    <SysDialog
+      :title="dialog.title"
+      :width="dialog.width"
+      :height="dialog.height"
+      :visible="dialog.visible"
+      @on-close="onClose"
+      @on-confirm="commit"
+    >
+      <template v-slot:content>
+        <el-form
+          :model="addModel"
+          ref="addRef"
+          :rules="rules"
+          label-width="80px"
+          :inline="false"
+          size="default"
+        >
+          <el-form-item prop="roleName" label="⻆色名称">
+            <el-input v-model="addModel.roleName"></el-input>
+          </el-form-item>
+          <el-form-item prop="remark" label="备注">
+            <el-input v-model="addModel.remark"></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
+    </SysDialog>
+  </el-main>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import SysDialog from '@/components/SysDialog.vue'
+import useDialog from '@/hooks/useDialog'
+import { FormInstance } from 'element-plus'
+//表单ref属性
+const addRef = ref<FormInstance>()
+//弹框属性
+const { dialog, onClose, onShow } = useDialog()
+//表单绑定的对象
+const searchParm = reactive({
+  currentPage: 1,
+  pageSize: 10,
+  roleName: ''
+})
+//新增按钮点击事件
+const addBtn = () => {
+  dialog.title = '新增角色'
+  dialog.height = 180
+  //显示弹框
+  onShow()
+}
+//新增表单对象
+const addModel = reactive({
+  roleId: '',
+  roleName: '',
+  remark: ''
+})
+//表单验证规则
+const rules = reactive({
+  roleName: [
+    {
+      required: true,
+      message: '请输入角色名称',
+      trigger: 'change'
+    }
+  ]
+})
+//表单提交
+const commit = () => {
+  addRef.value?.validate((valid) => {
+    if (valid) {
+      console.log('表单验证通过')
+    }
+  })
+}
+//搜索
+const searchBtn = () => {}
+//重置
+const resetBtn = () => {}
+</script>
