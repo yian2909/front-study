dd100a73373e1e9fd77d135d0bc9dbd553adc0f0
yian <915826506@qq.com>
2024-11-11 15:02:59 +0800

    feat: 安装 element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,26 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <h1>Hello, Element Plus!</h1>
+    <el-button type="primary" @click="handleClick">点击我</el-button>
+    <div style="margin-top: 20px; font-size: 32px">
+      <el-icon>
+        <Plus />
+      </el-icon>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script setup lang="ts">
+import { ElButton, ElIcon } from 'element-plus'
+import { Plus } from '@element-plus/icons-vue'
+
+const handleClick = () => {
+  console.log('按钮被点击')
+}
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+h1 {
+  color: #42b983;
 }
 </style>
