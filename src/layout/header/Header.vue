34d8c61cb4df8f6e7e67603d5b78f4be3a74c9ad
yian <915826506@qq.com>
2024-11-13 21:49:29 +0800

    feat: 个人中心头像布局

--- src/layout/header/Header.vue
+++ src/layout/header/Header.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="header-container">
-    <Collapse></Collapse>
-    <BreadCrumb></BreadCrumb>
+    <div class="left">
+      <Collapse></Collapse>
+      <BreadCrumb></BreadCrumb>
+    </div>
+
+    <div class="right">
+      <span style="padding-right: 15px; color: #fff">欢迎您,{{ name }}</span>
+      <LoginOut></LoginOut>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Collapse from './Collapse.vue'
 import BreadCrumb from './BreadCrumb.vue'
+import LoginOut from './LoginOut.vue'
+import { useUserStore } from '@/store/user'
+import { computed } from 'vue'
+const store = useUserStore()
+const name = computed(() => {
+  return store.getNickName
+})
 </script>
 
 <style scoped lang="scss">
 .header-container {
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  .left {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
---
+++ src/layout/header/LoginOut.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-dropdown placement="bottom-start">
+    <span class="el-dropdown-link">
+      <img class="userimg" src="@/assets/user.gif" />
+    </span>
+
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>修改密码</el-dropdown-item>
+        <el-dropdown-item>退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.el-dropdown-link:focus {
+  outline: none;
+}
+.userimg {
+  height: 45px;
+  width: 45px;
+  border-radius: 50%;
+  cursor: pointer;
+  background-color: #fff;
+}
+</style>
